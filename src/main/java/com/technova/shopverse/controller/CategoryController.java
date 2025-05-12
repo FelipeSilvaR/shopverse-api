9f00f46e9740357a0fd5d5c2cb05a47cd867e5ae
Felipe <felipe.silvar85@gmail.com>
2025-05-11 20:01:07 -0400

    Modulo 6 Completo

--- src/main/java/com/technova/shopverse/controller/CategoryController.java
+++ src/main/java/com/technova/shopverse/controller/CategoryController.java
@@ -1,10 +1,13 @@
 package com.technova.shopverse.controller;
 
 import com.technova.shopverse.model.Category;
+import com.technova.shopverse.model.Product;
 import com.technova.shopverse.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,33 +18,62 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping
-    public List<Category> getAll(){
+    public ResponseEntity<List<Category>> getAll(){
 
-        return categoryService.getAllCategories();
+        List<Category> categories = categoryService.getAllCategories();
+
+        if(categories.isEmpty()){
+
+            return ResponseEntity.noContent().build(); //204
+        }else{
+
+            return ResponseEntity.ok(categories); //200
+        }
     }
 
     @GetMapping("/{id}")
-    public Category getById(@PathVariable Long id){
+    public ResponseEntity<Category> getById(@PathVariable Long id){
 
-        return categoryService.getCategoryById(id).orElse(null);
+        return categoryService.getCategoryById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build()); //200 o 404
     }
 
     @PostMapping
-    public Category create(@RequestBody Category category){
+    public ResponseEntity<Category> create(@RequestBody Category category){
 
-        return categoryService.createCategory(category);
+        try{
+            Category createCategory = categoryService.createCategory(category);
+            URI location = URI.create("/api/categories" + createCategory.getId());
+
+            return ResponseEntity.created(location).body(createCategory); //201
+        }catch (IllegalArgumentException e){
+
+            return ResponseEntity.badRequest().build(); //400
+        }
     }
 
     @PutMapping("/{id}")
-    public Category update(@PathVariable Long id, @RequestBody Category category){
+    public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category category){
 
-        return categoryService.updateCategory(id, category);
+        try{
+            Category updateCategory = categoryService.updateCategory(id, category);
+
+            return ResponseEntity.ok(updateCategory); //200
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.notFound().build(); //404
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id){
 
-        categoryService.deleteCategory(id);
+        try{
+            categoryService.deleteCategory(id);
+
+            return ResponseEntity.noContent().build(); //204
+        }catch (IllegalArgumentException e){
+
+            return ResponseEntity.notFound().build(); //404
+        }
     }
 
 }
--- src/main/java/com/technova/shopverse/controller/ProductController.java
+++ src/main/java/com/technova/shopverse/controller/ProductController.java
@@ -3,8 +3,10 @@ package com.technova.shopverse.controller;
 import com.technova.shopverse.model.Product;
 import com.technova.shopverse.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,34 +17,63 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
-    public List<Product> getAll() {
+    public ResponseEntity<List<Product>> getAll() {
 
-        return productService.getAllProducts();
+        List<Product> products = productService.getAllProducts();
+
+        if(products.isEmpty()){
+
+            return ResponseEntity.noContent().build(); //204
+        }else{
+
+            return ResponseEntity.ok(products); //200
+        }
     }
 
     @GetMapping("/{id}")
-    public Product getById(@PathVariable Long id){
+    public ResponseEntity<Product> getById(@PathVariable Long id){
 
-        return productService.getProductById(id).orElse(null);
+        return productService.getProductById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build()); //200 o 404
     }
 
     @PostMapping
-    public Product create(@RequestBody Product product){
+    public ResponseEntity<Product> create(@RequestBody Product product){
 
-        return productService.createProduct(product);
+        try{
+            Product createProduct = productService.createProduct(product);
+            URI location = URI.create("/api/products" + createProduct.getId());
+
+            return ResponseEntity.created(location).body(createProduct); //201
+        }catch (IllegalArgumentException e){
+
+            return ResponseEntity.badRequest().build(); //400
+        }
     }
 
     @PutMapping("/{id}")
-    public Product update(@PathVariable Long id, @RequestBody Product product){
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product){
 
-        return productService.updateProduct(id, product);
+        try{
+            Product updateProduct = productService.updateProduct(id, product);
+
+            return ResponseEntity.ok(updateProduct); //200
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.notFound().build(); //404
+        }
+
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id){
 
-        productService.deleteProduct(id);
+        try{
+            productService.deleteProduct(id);
 
+            return ResponseEntity.noContent().build(); //204
+        }catch (IllegalArgumentException e){
+
+            return ResponseEntity.notFound().build(); //404
+        }
     }
 
 }
